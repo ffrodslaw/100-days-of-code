2ef3467b63b44fade3fdd1292de38416ab3e63b6
Anna <anna.walsdorff@protonmail.com>
2023-02-24 12:27:57 -0500

    Add highscore save file

--- 20_snake-game/main.py
+++ 20_snake-game/main.py
@@ -37,14 +37,18 @@ while game_on:
 
     # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+        # game_on = False
+        # scoreboard.game_over()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+            # game_on = False
+            # scoreboard.game_over()
 
 
 screen.exitonclick()
