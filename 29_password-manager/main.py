6b69b7f7c2d5bf1e2258cb5445dab034118bcc4c
Anna <anna.walsdorff@protonmail.com>
2023-03-03 15:19:13 -0500

    Handle exceptions

--- 29_password-manager/main.py
+++ 29_password-manager/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+import json
 # import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -31,19 +32,51 @@ def save_entry():
     website = entry_website.get()
     email = entry_email.get()
     pw = entry_pw.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": pw,
+        }
+    }
 
     if len(website) == 0 or len(pw) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!", icon="error")
     else:
-        # check if details are correct
-        ok = messagebox.askokcancel(title=website,
-                               message=f"These are the details entered: \nEmail: {email} \nPassword: {pw}")
-        if ok:
-            entry = f"{website} | {email} | {pw} \n"
-            with  open("data.txt", "a") as data:
-                data.write(entry)
+        try:
+            with open("data.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent = 4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # saving new data
+                json.dump(data, data_file, indent=4)
+        finally:
             entry_website.delete(0, "end")
             entry_pw.delete(0, "end")
+
+def load_entry():
+            website = entry_website.get()
+            try:
+                with open("data.json", "r") as data_file:
+                    # reading old data
+                    data = json.load(data_file)
+                    entry = data[website]
+            except FileNotFoundError:
+                messagebox.showerror(title="Oops", message="Database empty.")
+            except KeyError:
+                messagebox.showerror(title="Oops", message=f"Details for {website} are not in database.")
+            else:
+                email = entry["email"]
+                pw = entry["password"]
+
+                messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {pw}")
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -66,8 +99,8 @@ label_email.grid(column=0, row=2)
 label_pw = tk.Label(text="Password:")
 label_pw.grid(column=0, row=3)
 
-entry_website = tk.Entry(width=35)
-entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")  # sticky argument needed to line up grid nicely
+entry_website = tk.Entry(width=21)
+entry_website.grid(column=1, row=1, sticky="EW")  # sticky argument needed to line up grid nicely
 entry_website.focus()
 
 entry_email = tk.Entry(width=35)
@@ -76,6 +109,9 @@ entry_email.grid(column=1, row=2, columnspan=2, sticky="EW")
 
 entry_pw = tk.Entry(width=21)
 entry_pw.grid(column=1, row=3, sticky="EW")
+
+button_search = tk.Button(text="Search", command=load_entry)
+button_search.grid(column=2, row=1, sticky="EW")
 
 button_generate = tk.Button(text="Generate Password", command=generate_pw)
 button_generate.grid(column=2, row=3, sticky="EW")
