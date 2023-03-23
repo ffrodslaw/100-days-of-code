f438bdc23fc59920b36161f2bb89236b67ae5dc3
Anna <anna.walsdorff@protonmail.com>
2023-03-22 21:35:00 -0400

    typo

--- 47_amazon-price-tracker/main.py
+++ 47_amazon-price-tracker/main.py
@@ -5,11 +5,12 @@ from bs4 import BeautifulSoup
 import smtplib
 import os
 
-# # change these environmental variables and the link to the product
+# # change these environmental variables, the link to the product, and below which price you want to be notified
 my_email = os.getenv("GMAIL_USER")
 email_pw = os.getenv("GMAIL_PASSWORD")
 email_to = os.getenv("GMAIL_USER")
 product_link = "https://www.amazon.com/dp/B0BP6GMQ8S"
+price_threshold = 15
 
 header = {
     "Accept-Language": "en-US,en;q=0.9",
@@ -22,7 +23,7 @@ soup = BeautifulSoup(response, "lxml")
 title = soup.title.get_text()
 price = float(soup.find("span", class_="a-offscreen").get_text().replace("$", ""))
 
-if price < 15:
+if price < price_threshold:
     email_text = f"Subject: Amazon price alert!\n\nThe item {title} is now only ${price}.\nGet it here: {product_link}"
     email_encoded = email_text.encode("utf-8")
     print(email_encoded)
