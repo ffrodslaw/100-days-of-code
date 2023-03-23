330b31c52a82d392f7c39152882d699548f4ceed
Anna <anna.walsdorff@protonmail.com>
2023-03-22 21:31:07 -0400

    Day 47

---
+++ 47_amazon-price-tracker/main.py
@@ -0,0 +1,35 @@
+# Day 47: Amazon Price Tracker
+
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+import os
+
+# # change these environmental variables and the link to the product
+my_email = os.getenv("GMAIL_USER")
+email_pw = os.getenv("GMAIL_PASSWORD")
+email_to = os.getenv("GMAIL_USER")
+product_link = "https://www.amazon.com/dp/B0BP6GMQ8S"
+
+header = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Defined",
+}
+
+response = requests.get(product_link, headers=header).text
+soup = BeautifulSoup(response, "lxml")
+
+title = soup.title.get_text()
+price = float(soup.find("span", class_="a-offscreen").get_text().replace("$", ""))
+
+if price < 15:
+    email_text = f"Subject: Amazon price alert!\n\nThe item {title} is now only ${price}.\nGet it here: {product_link}"
+    email_encoded = email_text.encode("utf-8")
+    print(email_encoded)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=email_pw)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email_to,
+            msg=email_encoded)
