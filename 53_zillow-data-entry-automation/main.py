3ede8377031332f6379b2e8c7fffb83ce7da8411
Anna <anna.walsdorff@protonmail.com>
2023-03-30 19:38:54 -0400

    Day 53

---
+++ 53_zillow-data-entry-automation/main.py
@@ -0,0 +1,60 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from time import sleep
+
+zillow_url = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.69219435644531%2C%22east%22%3A-122.17446364355469%2C%22south%22%3A37.703343724016136%2C%22north%22%3A37.847169233586946%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D"
+form_url = os.environ["google_form"]
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
+
+response = requests.get(zillow_url, headers=header).text
+soup = BeautifulSoup(response, "lxml")
+
+listings = soup.find_all("div", {"class" : re.compile("StyledPropertyCardDataWrapper.* ")})
+
+urls = []
+addresses = []
+prices = []
+
+for listing in listings:
+    url_element = listing.find("a", class_="property-card-link")
+    if url_element["href"].startswith("/"):
+        url = f"http://www.zillow.com{url_element['href']}"
+    else:
+        url = url_element["href"]
+    urls.append(url)
+
+    address = url_element.find("address").text
+    addresses.append(address)
+
+    price = url_element.find_next("span").text
+    prices.append(price)
+
+# inputting into Google form
+
+CHROME_DRIVER_PATH = "/home/anna/Applications/chromedriver"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(service=Service(CHROME_DRIVER_PATH), options=chrome_options)
+
+for i in range(len(listings)-1):
+    driver.get(form_url)
+    sleep(1)
+    address_input = driver.find_element(By.XPATH,  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+    address_input.send_keys(addresses[i])
+    price_input.send_keys(prices[i])
+    link_input.send_keys(urls[i])
+    submit.click()
